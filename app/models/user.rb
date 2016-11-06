74e8d1143d6b7ba6ef0201e932462567fd2bb3f9
Marco <marco_bonetti@uol.com.br>
2016-11-06 00:50:15 +0000

    rodando

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
 
-has_secure_password
-validates :password, presence: true, length: { minimum: 6 }
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
 end
