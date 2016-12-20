80294d9b29b6c4873f906d19ddbec897b613166a
Marco Bonetti <marco_bonetti@uol.com.br>
2016-12-20 16:45:01 -0200

    com as paginas estaticas voltando ao idioma certo

--- app/controllers/espanhol_controller.rb
+++ app/controllers/espanhol_controller.rb
@@ -1,51 +1,99 @@
 class EspanholController < ApplicationController
     
      def home
+ 		@idioma=Idioma.find(1)
+		@idioma.aux = 2
+		@idioma.save
     end
     
     def calendarioematricula
+ 		@idioma=Idioma.find(1)
+		@idioma.aux = 2
+		@idioma.save
     end
     
     def egress
+ 		@idioma=Idioma.find(1)
+		@idioma.aux = 2
+		@idioma.save
     end
     
     def manualdoaluno
+ 		@idioma=Idioma.find(1)
+		@idioma.aux = 2
+		@idioma.save
     end
     
     def finais
+ 		@idioma=Idioma.find(1)
+		@idioma.aux = 2
+		@idioma.save
     end
     
     def quemsomos
+ 		@idioma=Idioma.find(1)
+		@idioma.aux = 2
+		@idioma.save
     end
     
     def grupos
+ 		@idioma=Idioma.find(1)
+		@idioma.aux = 2
+		@idioma.save
     end
     
     def linhas
+ 		@idioma=Idioma.find(1)
+		@idioma.aux = 2
+		@idioma.save
     end
     
-    def regimento
+    def regimentos
+ 		@idioma=Idioma.find(1)
+		@idioma.aux = 2
+		@idioma.save
     end
     
     def editais
+ 		@idioma=Idioma.find(1)
+		@idioma.aux = 2
+		@idioma.save
     end
     
     def estruturacurricular
+ 		@idioma=Idioma.find(1)
+		@idioma.aux = 2
+		@idioma.save
     end
     
     def legislacao
+ 		@idioma=Idioma.find(1)
+		@idioma.aux = 2
+		@idioma.save
     end
     
     def colaboradores
+ 		@idioma=Idioma.find(1)
+		@idioma.aux = 2
+		@idioma.save
     end
     
     def financas
+ 		@idioma=Idioma.find(1)
+		@idioma.aux = 2
+		@idioma.save
     end
     
     def formularios 
+ 		@idioma=Idioma.find(1)
+		@idioma.aux = 2
+		@idioma.save
     end
     
     def quadrodehorarios
+ 		@idioma=Idioma.find(1)
+		@idioma.aux = 2
+		@idioma.save
     end
     
     
