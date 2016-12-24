8f0efe668bf357e322e73b3e827d68a959a1e8d6
Marco <marco_bonetti@uol.com.br>
2016-12-24 12:02:32 +0000

    idiomas funcionando com session

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,7 +3,7 @@ module SessionsHelper
   # funções que determinam língua
 	
 	def ingles()
-	    @idioma=Idioma.find(1)
+		@idioma=Idioma.find(1)
 		@idioma.aux=1
 		@idioma.save
 	end
@@ -20,6 +20,75 @@ module SessionsHelper
 		@idioma.save
 	end
 	
+	
+	# funções que chamam páginas estáticas em portugues
+	
+	def portugues_home
+		troca(3)
+		portugues()
+		redirect_to home_path
+	end
+
+	def portugues_calendario
+		troca(3)
+		portugues()
+		redirect_to calendarioematricula_path
+	end
+	
+	def portugues_manual
+		troca(3)
+		portugues()
+		redirect_to manualdoaluno_path
+	end
+	
+	def portugues_quem
+		troca(3)
+		portugues()
+		redirect_to quemsomos_path
+	end
+	
+	def portugues_regimento
+		troca(3)
+		portugues()
+		redirect_to regimentos_path
+	end
+	
+	def portugues_editais
+		troca(3)
+		portugues()
+		redirect_to editais_path
+	end
+	
+	def portugues_curriculo
+		troca(3)
+		portugues()
+		redirect_to estruturacurricular_path
+	end
+	
+	def portugues_legislacao
+		troca(3)
+		portugues()
+		redirect_to legislacao_path
+	end
+	
+	def portugues_formulario
+		troca(3)
+		portugues()
+		redirect_to formularios_path
+	end
+	
+	def portugues_quadro
+		troca(3)
+		portugues()
+		redirect_to quadrodehorarios_path
+	end
+	
+	def portugues_egre
+		troca(3)
+		portugues()
+		redirect_to egress_path
+	end
+	
 	# funções que chamam páginas estáticas em inglês
 	
 	def ingles_home
@@ -29,51 +98,61 @@ module SessionsHelper
 	end
 
 	def ingles_calendario
+		troca(1)
 		ingles()
 		redirect_to calendarioematriculaing_path
 	end
 	
 	def ingles_manual
+		troca(1)
 		ingles()
 		redirect_to manualdoalunoing_path
 	end
 	
 	def ingles_quem
+		troca(1)
 		ingles()
 		redirect_to quemsomosing_path
 	end
 	
 	def ingles_regimento
+		troca(1)
 		ingles()
 		redirect_to regimentosing_path
 	end
 	
 	def ingles_editais
+		troca(1)
 		ingles()
 		redirect_to editaising_path
 	end
 	
 	def ingles_curriculo
+		troca(1)
 		ingles()
 		redirect_to estruturacurricularing_path
 	end
 	
 	def ingles_legislacao
+		troca(1)
 		ingles()
 		redirect_to legislacaoing_path
 	end
 	
 	def ingles_formulario
+		troca(1)
 		ingles()
 		redirect_to formulariosing_path
 	end
 	
 	def ingles_quadro
+		troca(1)
 		ingles()
 		redirect_to quadrodehorariosing_path
 	end
 	
 	def ingles_egre
+		troca(1)
 		ingles()
 		redirect_to egressing_path
 	end
@@ -88,74 +167,67 @@ module SessionsHelper
 	end
 
 	def espanhol_calendario
+		troca(2)
 		espanhol()
 		redirect_to calendarioematriculaesp_path
 	end
 	
 	def espanhol_manual
+		troca(2)
 		espanhol()
 		redirect_to manualdoalunoesp_path
 	end
 	
 	def espanhol_quem
+		troca(2)
 		espanhol()
 		redirect_to quemsomosesp_path
 	end
 	
 	def espanhol_regimento
+		troca(2)
 		espanhol()
 		redirect_to regimentosesp_path
 	end
 	
 	def espanhol_editais
+		troca(2)
 		espanhol()
 		redirect_to editaisesp_path
 	end
 	
 	def espanhol_curriculo
+		troca(2)
 		espanhol()
 		redirect_to estruturacurricularesp_path
 	end
 	
 	def espanhol_legislacao
+		troca(2)
 		espanhol()
 		redirect_to legislacaoesp_path
 	end
 	
 	def espanhol_formulario
+		troca(2)
 		espanhol()
 		redirect_to formulariosesp_path
 	end
 	
 	def espanhol_quadro
+		troca(2)
 		espanhol()
 		redirect_to quadrodehorariosesp_path
 	end
 	
 	def espanhol_egre
+		troca(2)
 		espanhol()
 		redirect_to egressesp_path
 	end
 	
 #outras funções
-	def troca(arg)
-	if !logged_in?
-		numero=Visitante.count
-		visitante=Visitante.new
-		visitante.idioma=arg
-		if numero>=100
-			for i in ( 3..100 )
-				apaga=Visitante.find(i)
-				apaga.delete
-			end
-			visitante.id=3
-		else 
-			visitante.id=numero+1
-		end
-		visitante.save
-	    session[:visitante_id] = visitante.id
-		end
-	end
+
 
 	def portugues_home
 		troca(3)
@@ -169,218 +241,260 @@ module SessionsHelper
 	end
 	
 	def ingles_colaboradores()
+		troca(1)
 		ingles()
 		redirect_to colaboradores_url
 	end
 	
 	def ingles_contato()
+		troca(1)
 		ingles()
 		redirect_to contatos_url
 	end
 
 	def ingles_finais()
+		troca(1)
 		ingles()
 		redirect_to finais_url
 	end
 	
 	def ingles_grupos()
+		troca(1)
 		ingles()
 		redirect_to grupos_url
 	end
 	
 	def ingles_linhas()
+		troca(1)
 		ingles()
 		redirect_to linhas_url
 	end
 	
 	def ingles_financas()
+		troca(1)
 		ingles()
 		redirect_to financas_url
 	end
 	
 	def ingles_links()
+		troca(1)
 		ingles()
 		redirect_to links_url
 	end
 	
 	def ingles_publicas()
+		troca(1)
 			ingles()
 			redirect_to publicas_url
 	end
 	
 	def ingles_eventos()
+		troca(1)
 			ingles()
 			redirect_to eventos_url
 	end
 
 	
 	def ingles_seletivos()
+		troca(1)
 		ingles()
 		redirect_to processo_seletivos_url
 	end
 	
 	def ingles_docentes()
+		troca(1)
 		ingles()
 		redirect_to docentes_url
 	end
 	
 		
 	def ingles_disciplinas()
+		troca(1)
 		ingles()
 		redirect_to disciplinas_url
 	end
 
 	def ingles_disciplinas2s()
+		troca(1)
 		ingles()
 		redirect_to disciplinas2s_url
 	end
 	
 	def ingles_disciplinas3s()
+		troca(1)
 		ingles()
 		redirect_to disciplinas3s_url
 	end
 	
 	def espanhol_contato()
+		troca(2)
 		espanhol()
 		redirect_to contatos_url
 	end
 
 	def espanhol_disciplinas3s()
+		troca(2)
 		espanhol()
 		redirect_to disciplinas3s_url
 	end
 	
 	def espanhol_disciplinas2s()
+		troca(2)
 		espanhol()
 		redirect_to disciplinas2s_url
 	end
 	
 	def espanhol_disciplinas()
+		troca(2)
 		espanhol()
 		redirect_to disciplinas_url
 	end
 	
 	
 	def espanhol_docentes()
+		troca(2)
 		espanhol()
 		redirect_to docentes_url
 	end
 	
 	
 	def espanhol_seletivos()
+		troca(2)
 		espanhol()
 		redirect_to processo_seletivos_url
 	end
 
 	
 	def espanhol_eventos()
+		troca(2)
 		espanhol()
 		redirect_to eventos_url
 	end
 	
 	def espanhol_publicas()
+		troca(2)
 		espanhol()
 		redirect_to publicas_url
 	end
 	def espanhol_links()
+		troca(2)
 		espanhol()
 		redirect_to links_url
 	end
 
 	def espanhol_colaboradores()
+		troca(2)
 		espanhol()
 		redirect_to colaboradores_url
 	end
 	
 	def espanhol_finais()
+		troca(2)
 		espanhol()
 		redirect_to finais_url
 	end
 	
 	def espanhol_grupos()
+		troca(2)
 		espanhol()
 		redirect_to grupos_url
 	end
 	
 	def espanhol_linhas()
+		troca(2)
 		espanhol()
 		redirect_to linhas_url
 	end
 	
 	def espanhol_financas()
+		troca(2)
 		espanhol()
 		redirect_to financas_url
 	end
 # Portugues funçoes de chamada das dinâmicas
 
 	def portugues_colaboradores()
+		troca(3)
 		portugues()
 		redirect_to colaboradores_url
 	end
 	
 	def portugues_contato()
+		troca(3)
 		portugues()
 		redirect_to contatos_url
 	end
 
 	def portugues_finais()
+		troca(3)
 		portugues()
 		redirect_to finais_url
 	end
 	
 	def portugues_grupos()
+		troca(3)
 		portugues()
 		redirect_to grupos_url
 	end
 	
 	def portugues_linhas()
+		troca(3)
 		portugues()
 		redirect_to linhas_url
 	end
 	
 	def portugues_financas()
+		troca(3)
 		portugues()
 		redirect_to financas_url
 	end
 	
 	def portugues_links()
+		troca(3)
 		portugues()
 		redirect_to links_url
 	end
 	
 	def portugues_publicas()
+		troca(3)
 			portugues()
 			redirect_to publicas_url
 	end
 	
 	def portugues_eventos()
+		troca(3)
 			portugues()
 			redirect_to eventos_url
 	end
 
 	
 	def portugues_seletivos()
+		troca(3)
 		portugues()
 		redirect_to processo_seletivos_url
 	end
 	
 	def portugues_docentes()
+		troca(3)
 		portugues()
 		redirect_to docentes_url
 	end
 	
 		
 	def portugues_disciplinas()
+		troca(3)
 		portugues()
 		redirect_to disciplinas_url
 	end
 
 	def portugues_disciplinas2s()
+		troca(3)
 		portugues()
 		redirect_to disciplinas2s_url
 	end
 	
 	def portugues_disciplinas3s()
+		troca(3)
 		portugues()
 		redirect_to disciplinas3s_url
 	end
@@ -388,6 +502,21 @@ module SessionsHelper
 
 
 #Demais funções
+
+def get_idioma()
+  	@visitante = Visitante.find_by(id: session[:visitante_id])
+  	return @visitante
+end
+
+def troca(arg)
+		if !logged_in?
+			session.delete(10)
+			session.delete(20)
+			session.delete(30)
+			session[:visitante_id] = arg*10
+		end
+	end
+
   def log_in(user)
     session[:user_id] = user.id
   end
